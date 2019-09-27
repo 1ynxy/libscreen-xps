2c63e2127905e362c4e6c4cbd16631bf613d4697
char <lynxy@lynxdev.xyz>
2019-09-27 23:44:51 +0100

    fixed build file for both

--- build.sh
+++ build.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+# create build folder
+
 [[ -d build ]] || mkdir -p build
 
-gcc -O2 -o build/screencon src/screencon.c
+# delete existing builds
 
-if [ ! -f ./build/screencon ]; then
-    echo "executable not built correctly"
-    exit
-fi
+[[ -f build/screenconnect ]] && rm build/screenconnect
+[[ -f build/screenorient ]] && rm build/screenorient
 
-chmod u+x build/screencon
+# build
 
-# install
+gcc -O2 -o build/screenconnect src/screenconnect.c
+gcc -O2 -o build/screenorient src/screenorient.c
 
-[[ $# > 0 ]] && [[ $1 == "run" ]] && build/screencon
+# exit if failed
 
-[[ $# > 0 ]] && [[ $1 == "install" ]] && sudo cp build/screencon /usr/local/bin/screencon
+[[ -f build/screenconnect ]] && [[ -f build/screenorient ]] && exit
+
+chmod u+x build/screenconnect
+chmod u+x build/screenorient
+
+# run or install
+
+[[ $# > 0 ]] && [[ $1 == "run" ]] && build/screenconnect &
+[[ $# > 0 ]] && [[ $1 == "run" ]] && build/screenorient &
+
+[[ $# > 0 ]] && [[ $1 == "install" ]] && sudo cp build/screenconnect /usr/local/bin/screenconnect
+[[ $# > 0 ]] && [[ $1 == "install" ]] && sudo cp build/screenorient /usr/local/bin/screenorient
